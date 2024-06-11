8e014df4a05ab131f88a9e7dda7f7537a4bfd645
ericaokamura <erica.okamura@yahoo.com.br>
2024-06-11 18:44:33 -0300

    Initial Commit

---
+++ fiap-app-ios/fiap_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiap_app_iosApp.swift
+//  fiap-app-ios
+//
+//  Created by Erica Okamura on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct fiap_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
